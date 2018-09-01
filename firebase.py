96a1a42e66f75b2bbb24cadb273bced8e00c73e9
Bastián Véliz Vega <bastianX6@gmail.com>
2018-09-01 04:07:54 -0300

    Download files from Firebase Cloud Storage

--- firebase.py
+++ firebase.py
@@ -1,12 +1,14 @@
 import firebase_admin
 import json
+import logging
 from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import storage
+from pathlib import Path
 
-class FirebaseDatabse(object):
+class FirebaseManager(object):
 
-    def __init__(self):
+    def __init__(self, songsFolder):
         '''
         Constructor
         '''
@@ -19,8 +21,9 @@ class FirebaseDatabse(object):
             'storageBucket': '{}.appspot.com'.format(self.firebaseData['cloud_storage']['bucket_name'])
         })
         self.db = firestore.client()
-        self.bucket = storage.bucket() 
+        self.bucket = storage.bucket()
         self.songsReference = self.db.collection(self.firebaseData['firestore']['collection_name'])
+        self.songsFolder = songsFolder
 
     def getData(self):
         docs = self.songsReference.order_by(self.firebaseData['firestore']['field_name']).limit(1).get()
@@ -28,5 +31,24 @@ class FirebaseDatabse(object):
             return doc.to_dict()
 
     def getSongsFiles(self):
-        pass
+        prefix = self.firebaseData['cloud_storage']['file_prefix']
+        blobs = self.bucket.list_blobs(prefix = prefix)
+        for blob in blobs:
+            fileName = blob.name.replace(prefix, '')
+            if len(fileName) == 0:
+                continue
+
+            localPath = self.songsFolder+u'/'+fileName
+            logging.debug("fileName: %s", fileName)
+            if not Path(localPath).exists():
+                logging.warning("File %s doesn't exists. Downloading...", fileName)
+                self.download_blob(blob.name, localPath)
+
+    def download_blob(self, source_blob_name, destination_file_name):
+        """Downloads a blob from the bucket."""
+        blob = self.bucket.blob(source_blob_name)
+
+        blob.download_to_filename(destination_file_name)
+        logging.info('Sound %s downloaded to %s.', source_blob_name, destination_file_name)
+            
     
--- main.py
+++ main.py
@@ -8,16 +8,17 @@ gi.require_version('Gst', '1.0')
 from gi.repository import Gst
 from gi.repository import GLib
 from gst import MediaPlayer
-from firebase import FirebaseDatabse
+from firebase import FirebaseManager
 from pathlib import Path
 
 class Main(object):
 
     def __init__(self):
         self.loop = asyncio.get_event_loop()
-        self.firebaseDatabase = FirebaseDatabse()
+        self.songsFolder = os.getcwd()+'/songs'
+        self.firebaseDatabase = FirebaseManager(self.songsFolder)
         self.data = dict()
-        self.currentFolder = os.getcwd()+'/songs'
+        
         os.putenv('GST_DEBUG_DUMP_DOT_DIR',os.getcwd()+'/dot')
         os.putenv('GST_DEBUG', '2')
         logging.basicConfig(level = logging.INFO)
@@ -27,12 +28,15 @@ class Main(object):
     async def fetchData(self):
         self.data = self.firebaseDatabase.getData()
 
+    async def fetchFiles(self):
+        self.firebaseDatabase.getSongsFiles()
+
     def processInput(self):
         try:
             console_input = input("Song code: ")
             song_name = main.data['songs'][console_input]
-            song_path = main.currentFolder+u'/'+song_name
-            if Path(main.currentFolder+u'/'+song_name).exists():
+            song_path = main.songsFolder+u'/'+song_name
+            if Path(main.songsFolder+u'/'+song_name).exists():
                 logging.debug("Path: {}".format(song_path))
                 mediaPlayer = MediaPlayer()
                 mediaPlayer.play_sound(song_path, self.onPlayingEnded)
@@ -47,7 +51,8 @@ class Main(object):
 if __name__ == '__main__':
 
     main = Main()
-    main.loop.run_until_complete(main.fetchData())  
+    main.loop.run_until_complete(main.fetchData())
+    main.loop.run_until_complete(main.fetchFiles()) 
 
     if main.data:
         logging.debug("Data: {}".format(main.data))
