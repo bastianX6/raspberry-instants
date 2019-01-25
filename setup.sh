a3a64d0846d0e75d5bfbd4d5ebb8d18b053fe36b
koke <jpobleteriquelme@gmail.com>
2019-01-25 18:37:23 -0300

    [requirements] Added setup file which will install all brew and pip dependencies
    - requires sudo to install pip dependencies

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 brew install pulseaudio
 brew install pulseaudio-alsa
 brew install alsa-utils
@@ -5,3 +7,5 @@ brew install pygobject3
 brew install gst-plugins-base
 brew install gst-plugins-good
 brew install gst-plugins-bad
+
+pip install -r requirements.txt
