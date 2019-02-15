5fc09e9592459d958f717ad461ce3a89457d729b
Bastián Véliz Vega <bastianX6@gmail.com>
2019-02-15 15:41:41 -0300

    Add /song endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,34 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+var db = admin.firestore();
+
+app.post('/song', (req, res) => {
+
+    var Validator = require('jsonschema').Validator;
+    var v = new Validator();
+    var songSchema = {
+        "songCode": {"type": "string"},
+        "description": {"type": "string"},
+        "songName": {"type": "string"},
+        "public": {"type": "boolean"},
+    }
+    var song = req.body;
+    if(v.validate(req.body, songSchema).valid) {
+        db.collection("songs2").add(req.body)
+        .then( ref => { 
+            res.status(201).json("Song added successfully");
+        }).catch( err => {
+            res.status(400).json("Error: "+err);
+        });
+    } else {
+        res.status(400).json("Invalid JSON");
+    }
+
+});
+
+exports.botonera = functions.https.onRequest(app);
