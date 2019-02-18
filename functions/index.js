907f3211eb24bcd9f82180ec045626512c6ad80c
Bastián Véliz Vega <bastianX6@gmail.com>
2019-02-18 14:35:19 -0300

    Update /song endpoint to validate json schema
    - Add songs endpoint to get all songs as array

--- functions/index.js
+++ functions/index.js
@@ -12,23 +12,44 @@ app.post('/song', (req, res) => {
     var Validator = require('jsonschema').Validator;
     var v = new Validator();
     var songSchema = {
-        "songCode": {"type": "string"},
-        "description": {"type": "string"},
-        "songName": {"type": "string"},
-        "public": {"type": "boolean"},
-    }
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "id": "songSchema",
+        "type": "object",
+        "properties": {
+            "songCode": {"type": "string"},
+            "description": {"type": "string"},
+            "songName": {"type": "string"},
+            "public": {"type": "boolean"},
+        },
+        "required": [
+            "songCode", "songName", "public", "description"
+        ]
+    };
+
     var song = req.body;
-    if(v.validate(req.body, songSchema).valid) {
+    var isValid = v.validate(req.body, songSchema).valid;
+    if(isValid) {
         db.collection("songs2").add(req.body)
         .then( ref => { 
-            res.status(201).json("Song added successfully");
+            return res.status(201).json("Song added successfully");
         }).catch( err => {
-            res.status(400).json("Error: "+err);
+            return res.status(400).json("Error: "+err);
         });
     } else {
         res.status(400).json("Invalid JSON");
     }
+});
 
+app.get('/songs', (req, res) => {
+    db.collection("songs2").where("public", "==", true).get()
+    .then( snapshot => {
+        var docData = snapshot.docs.map( doc => {
+            return doc.data();
+        })
+        return res.status(200).json({"songs": docData});
+    }).catch(err => {
+        return res.status(400).json("Error getting documents: "+err);
+    });
 });
 
 exports.botonera = functions.https.onRequest(app);
